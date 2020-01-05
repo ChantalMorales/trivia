08e99eb6013a60bd2acb2f45d7f96d7bdd9e1896
Jonathan Vasquez Vivas <jon2013.p@hotmail.com>
2020-01-05 17:38:32 -0500

    #1-conexion a la base de datos

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvhPs1x56tXUTI39DFJqbvO5zXXOLhC9M",
+    authDomain: "quiz-c24e4.firebaseapp.com",
+    databaseURL: "https://quiz-c24e4.firebaseio.com",
+    projectId: "quiz-c24e4",
+    storageBucket: "quiz-c24e4.appspot.com",
+    messagingSenderId: "1061419858594",
+    appId: "1:1061419858594:web:ee34bc8c8cbbe9730db5fb",
+    measurementId: "G-64H5YF2H4Q"
+  };
+
+  firebase.initializeApp(config);
